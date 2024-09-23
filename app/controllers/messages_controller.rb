e62b9397e04a4f0f8d09e6501d2a8e41c96ab64e
christian0429 <christianmellor429@gmail.com>
2024-09-23 04:36:35 -0400

    Remove update function in message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,28 +20,6 @@ class MessagesController < ApplicationController
       render "chatrooms/show", status: :unprocessable_entity
     end
   end
-  
-  def update
-    @chatroom = Chatroom.find(params[:chatroom_id])
-    @message = Message.new(message_params)
-    @message.chatroom = @chatroom
-    if user_signed_in?
-      @message.user = current_user
-    elsif organization_signed_in?
-      @message.organization = current_organization
-    end
-    if @message.save
-      ChatroomChannel.broadcast_to(
-        @chatroom,
-        message: render_to_string(partial: "message", locals: { message: @message }),
-        sender_id: user_signed_in? ? @message.user.id : @message.organization.id
-      )
-      head :ok
-      authorize @message
-    else
-      render "chatrooms/show", status: :unprocessable_entity
-    end
-  end
 
   private
 
